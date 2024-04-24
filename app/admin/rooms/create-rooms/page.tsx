cd7c01b6aba3b35649ecbc3faae4429e554774d6
rinehaxor <rinehaxor@users.noreply.github.com>
2024-04-24 15:32:11 +0700

    add:styling page rooms

--- app/admin/rooms/create-rooms/page.tsx
+++ app/admin/rooms/create-rooms/page.tsx
@@ -2,13 +2,17 @@ import AddRoom from '@/components/admin/addRoom';
 import ReadRoom from '@/components/admin/readRoom';
 import NavbarAdmin from '@/components/admin/navbarAdmin';
 import React from 'react';
+import SideBar from '@/components/admin/SideBar';
 
 export default function page() {
    return (
       <div className="w-full">
          <div className="flex-1 w-full flex flex-col gap-20 items-center">
             <NavbarAdmin />
-            <AddRoom />
+            <div className="flex flex-row">
+               <SideBar />
+               <AddRoom />
+            </div>
          </div>
       </div>
    );
