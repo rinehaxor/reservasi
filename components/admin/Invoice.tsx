cf1777e2b955f58bbd3deebaba1de20a1f270706
rinehaxor <rinehaxor@users.noreply.github.com>
2024-06-09 19:21:25 +0700

    fix:pdf invoice

--- components/admin/Invoice.tsx
+++ components/admin/Invoice.tsx
@@ -1,89 +1,168 @@
-import { Page, Text, View, Document, StyleSheet, Image } from '@react-pdf/renderer';
+import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+import { format } from 'date-fns';
 
-// Refined styles
+// Refined styles to match the provided design
 const styles = StyleSheet.create({
    page: {
       flexDirection: 'column',
       backgroundColor: '#ffffff',
-      padding: 40,
+      padding: 20,
+      fontSize: 22,
    },
    section: {
       margin: 10,
-      padding: 20,
-      backgroundColor: '#f7f7f7',
-      borderRadius: 12,
-      shadow: '0 4px 8px rgba(0,0,0,0.1)',
-      flexGrow: 1,
-      fontSize: 12,
-      lineHeight: 1.5,
+      padding: 10,
+      fontSize: 10,
    },
    header: {
-      fontSize: 26,
-      marginBottom: 20,
+      fontSize: 16,
+      marginBottom: 10,
       textAlign: 'center',
       color: '#003366',
-      paddingBottom: 10,
       fontWeight: 'bold',
    },
-   subtitle: {
-      fontSize: 18,
-      margin: 10,
-      color: '#0056b3',
-      marginBottom: 5,
-      paddingBottom: 3,
-      borderBottom: '2px solid #0056b3',
+   label: {
+      fontSize: 10,
+      fontWeight: 'bold',
    },
    text: {
-      margin: 10,
-      fontSize: 14,
-      color: '#333333',
+      fontSize: 10,
       marginBottom: 5,
    },
-   image: {
-      marginBottom: 15,
-      width: 150,
-      height: 100,
-      borderRadius: 10,
+   contact: {
+      fontSize: 10,
+      fontWeight: 'bold',
+      color: '#003366',
+   },
+   table: {
+      width: '100%',
+      borderStyle: 'solid',
+      borderWidth: 1,
+      margin: 10,
+   },
+   tableRow: {
+      flexDirection: 'row',
+   },
+   tableColHeader: {
+      width: '50%',
+      borderStyle: 'solid',
+      borderWidth: 1,
+      backgroundColor: '#f0f0f0',
+      padding: 5,
+   },
+   tableCol: {
+      width: '50%',
+      borderStyle: 'solid',
+      borderWidth: 1,
+      padding: 5,
    },
    footer: {
-      marginTop: 25,
-      fontSize: 14,
+      marginTop: 20,
+      fontSize: 10,
       textAlign: 'center',
       color: '#666666',
    },
-   link: {
-      color: '#0066cc',
-      textDecoration: 'underline',
+   row: {
+      flexDirection: 'row',
+      marginBottom: 5,
+   },
+   cellLabel: {
+      width: '30%',
+      fontSize: 10,
+      fontWeight: 'bold',
+   },
+   cellText: {
+      width: '70%',
+      fontSize: 10,
    },
 });
 
+const formatDate = (dateString: Date) => {
+   return format(new Date(dateString), 'yyyy-MM-dd');
+};
 // Document component
 const MyDocument = ({ bookingData }: any) => (
    <Document>
-      <Page size="A4" style={styles.page}>
-         <Text style={styles.header}>Hotel Invoice</Text>
+      <Page size="A4" orientation="landscape" style={styles.page}>
+         <Text style={styles.header}>Booking Confirmation</Text>
 
          <View style={styles.section}>
-            <Text style={styles.subtitle}>Booking Details</Text>
-            <Text style={styles.text}>Invoice Number: {bookingData.invoice_number}</Text>
-            <Text style={styles.text}>Booking Date: {bookingData.bookingdate}</Text>
-            <Text style={styles.text}>Check-in Date: {bookingData.checkindate}</Text>
-            <Text style={styles.text}>Check-out Date: {bookingData.checkoutdate}</Text>
-            <Text style={styles.text}>Room Type: {bookingData.room.name}</Text>
-            <Text style={styles.text}>Total Price: {bookingData.total_price.toLocaleString('id-ID', { style: 'currency', currency: 'IDR' })}</Text>
-            <Text style={styles.text}>Booking Status: {bookingData.booking_status}</Text>
-            <Text style={styles.text}>Payment Status: {bookingData.payment_status}</Text>
+            <View style={styles.row}>
+               <Text style={styles.cellLabel}>Booking Invoice:</Text>
+               <Text style={styles.cellText}>{bookingData.invoice_number}</Text>
+            </View>
+            <View style={styles.row}>
+               <Text style={styles.cellLabel}>Name:</Text>
+               <Text style={styles.cellText}>{bookingData.name}</Text>
+            </View>
+            <View style={styles.row}>
+               <Text style={styles.cellLabel}>Email:</Text>
+               <Text style={styles.cellText}>{bookingData.email}</Text>
+            </View>
+            <View style={styles.row}>
+               <Text style={styles.cellLabel}>Nomer Telepone:</Text>
+               <Text style={styles.cellText}>{bookingData.phone_number}</Text>
+            </View>
+            <View style={styles.row}>
+               <Text style={styles.cellLabel}>Payment Status:</Text>
+               <Text style={styles.cellText}>{bookingData.payment_status}</Text>
+            </View>
+            <View style={styles.row}>
+               <Text style={styles.cellLabel}>Booking Status:</Text>
+               <Text style={styles.cellText}>{bookingData.booking_status}</Text>
+            </View>
 
-            <Text style={styles.subtitle}>Customer Details</Text>
-            <Text style={styles.text}>Name: {bookingData.name}</Text>
-            <Text style={styles.text}>
-               Email: <Text style={styles.link}>{bookingData.email}</Text>
-            </Text>
-            <Text style={styles.text}>Phone Number: {bookingData.phone_number}</Text>
-            <Text style={styles.text}>Payment Account Name: {bookingData.payment_name}</Text>
-            <Text style={styles.text}>Payment Account Number: {bookingData.payment_account_number}</Text>
+            <View style={styles.table}>
+               <View style={styles.tableRow}>
+                  <View style={styles.tableColHeader}>
+                     <Text style={styles.text}>Tgl Reservasi</Text>
+                  </View>
+                  <View style={styles.tableColHeader}>
+                     <Text style={styles.text}>Tipe Kamar</Text>
+                  </View>
+                  <View style={styles.tableColHeader}>
+                     <Text style={styles.text}> Nama Pembayar</Text>
+                  </View>
+                  <View style={styles.tableColHeader}>
+                     <Text style={styles.text}>Norek Pembayaran</Text>
+                  </View>
+                  <View style={styles.tableColHeader}>
+                     <Text style={styles.text}>Check-in</Text>
+                  </View>
+                  <View style={styles.tableColHeader}>
+                     <Text style={styles.text}>Check-out</Text>
+                  </View>
+                  <View style={styles.tableColHeader}>
+                     <Text style={styles.text}>Total</Text>
+                  </View>
+               </View>
+               <View style={styles.tableRow}>
+                  <View style={styles.tableCol}>
+                     <Text style={styles.text}>{formatDate(bookingData.bookingdate)}</Text>
+                  </View>
+                  <View style={styles.tableCol}>
+                     <Text style={styles.text}>{bookingData.room.name}</Text>
+                  </View>
+                  <View style={styles.tableCol}>
+                     <Text style={styles.text}>{bookingData.payment_name}</Text>
+                  </View>
+                  <View style={styles.tableCol}>
+                     <Text style={styles.text}>{bookingData.payment_account_number}</Text>
+                  </View>
+                  <View style={styles.tableCol}>
+                     <Text style={styles.text}>{bookingData.checkindate}</Text>
+                  </View>
+                  <View style={styles.tableCol}>
+                     <Text style={styles.text}>{bookingData.checkoutdate}</Text>
+                  </View>
+                  <View style={styles.tableCol}>
+                     <Text style={styles.text}>{bookingData.total_price.toLocaleString('id-ID', { style: 'currency', currency: 'IDR' })}</Text>
+                  </View>
+               </View>
+            </View>
+
+            <Text style={styles.footer}>Thank you for booking with us!</Text>
          </View>
-         <Text style={styles.footer}>Thank you for choosing us!</Text>
       </Page>
    </Document>
 );
